af73c702b9d21ce8cbe0ae032baf6f41a5a2fcf2
Michał Wróblewski <MasterWrobel@outlook.com>
2020-01-09 21:45:33 +0100

    Init commit

---
+++ UserService/Database/DatabaseContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using UserService.Database.Entities;
+
+namespace UserService.Database
+{
+    public class DatabaseContext:DbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
